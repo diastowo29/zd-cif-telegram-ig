bc8fdffa3cf30b70b2a3b8d6886f6cb7d8aa80e0
Diastowo <diastowo@gmail.com>
2017-11-14 17:00:19 +0700

    testing telethon

--- hello/views.py
+++ hello/views.py
@@ -22,6 +22,35 @@ tasks = [
 
 # Create your views here.
 def index(request):
+
+	api_id = 184365
+	api_hash = '640727dc57738548a9cbc23e5d8d1bbe'
+	phone = '+6281294059775'
+	client = TelegramClient('diastowo', api_id, api_hash)
+	client.connect()
+
+	if not client.is_user_authorized():
+		print('not logged in')
+		client.send_code_request(phone);
+		client.sign_in(phone, input('Enter the code: '));
+	else :
+		print('login success');
+
+	# dialogs, entities = client.get_dialogs(5)
+	# for entity in entities:
+	# 	print('firstname: ', entity.first_name, ' username: ', entity.username, ' user id: ', entity.id);
+
+	# result = client(GetHistoryRequest(
+	# 		entities[4],
+	# 		limit=20,
+	# 		offset_date=None,
+	# 	    offset_id=0,
+	# 	    max_id=0,
+	# 	    min_id=0,
+	# 	    add_offset=0
+	#     ));
+	# print(result.messages[0].message);
+
     return JsonResponse({'tasks':tasks})
     # return render(request, 'index.html')
 
