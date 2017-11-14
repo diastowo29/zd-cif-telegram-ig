72aacecd249353864c6a9dd5f5a3cd64a5e879e1
Diastowo <diastowo@gmail.com>
2017-11-14 16:21:04 +0700

    testing telethon

--- hello/views.py
+++ hello/views.py
@@ -8,14 +8,14 @@ from telethon import TelegramClient
 tasks = [
     {
         'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
+        'title': 'Buy groceries',
+        'description': 'Milk, Cheese, Pizza, Fruit, Tylenol', 
         'done': False
     },
     {
         'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web', 
+        'title': 'Learn Python',
+        'description': 'Need to find a good Python tutorial on the web', 
         'done': False
     }
 ]
