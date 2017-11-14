fa29393c8da6cda6bb5770dfc3475c30cb3bc157
Diastowo <diastowo@gmail.com>
2017-11-14 11:41:08 +0700

    second commit

--- hello/views.py
+++ hello/views.py
@@ -3,10 +3,26 @@ from django.http import HttpResponse
 
 from .models import Greeting
 
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web', 
+        'done': False
+    }
+]
+
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Python!')
-    return render(request, 'index.html')
+    return jsonify({'tasks': tasks})
+    # return render(request, 'index.html')
 
 
 def db(request):
