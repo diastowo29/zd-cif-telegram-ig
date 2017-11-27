f7772618ab568bd6f57bdb7f8377a6fa812b2470
Diastowo <diastowo@gmail.com>
2017-11-27 11:14:04 +0700

    pull

--- zendesk/form.py
+++ zendesk/form.py
@@ -2,16 +2,16 @@ from django import forms
 
 class ContactForm(forms.Form):
     name = forms.CharField(label='Integeration Name', max_length=100, widget=forms.TextInput(attrs={'class':'form-control'}))
-    api_id = forms.CharField(label='Api ID', max_length=100, widget=forms.TextInput(attrs={'class':'form-control'}))
-    api_hash = forms.CharField(label='Api Hash', max_length=100, widget=forms.TextInput(attrs={'class':'form-control'}))
+    api_id = forms.CharField(label='Api ID', widget=forms.TextInput(attrs={'class':'form-control'}))
+    api_hash = forms.CharField(label='Api Hash', widget=forms.TextInput(attrs={'class':'form-control'}))
     phone_number = forms.CharField(label='Phone Number', max_length=100, widget=forms.TextInput(attrs={'class':'form-control'}))
     username = forms.CharField(label='Username', max_length=100, widget=forms.TextInput(attrs={'class':'form-control'}))
 
 class MetaContactForm(forms.Form):
     # name = forms.CharField(label='', max_length=100, widget=forms.TextInput(attrs={'readonly':'readonly', 'class': 'form-control'}))
-    verify = forms.CharField(label = '', max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'id': 'verify'}))
+    verify = forms.CharField(label = '', widget=forms.TextInput(attrs={'class': 'form-control', 'id': 'verify'}))
     metadata = forms.CharField(label = '', widget=forms.TextInput(attrs={'readonly':'readonly', 'id': 'metadata', 'hidden': 'true'}))
-    return_url = forms.CharField(label = '', max_length=100, widget=forms.TextInput(attrs={'readonly':'readonly', 'id': 'return_url' , 'hidden': 'true'}))
+    return_url = forms.CharField(label = '', widget=forms.TextInput(attrs={'readonly':'readonly', 'id': 'return_url' , 'hidden': 'true'}))
     # api_id = forms.CharField(label='Api ID', max_length=100, widget=forms.TextInput(attrs={'readonly':'readonly'}))
     # api_hash = forms.CharField(label='Api Hash', max_length=100, widget=forms.TextInput(attrs={'readonly':'readonly'}))
     # phone_number = forms.CharField(label='Phone Number', max_length=100, widget=forms.TextInput(attrs={'readonly':'readonly'}))
@@ -19,4 +19,4 @@ class MetaContactForm(forms.Form):
 class SendMetaForm(forms.Form):
     name = forms.CharField(label='Account Name', max_length=100, widget=forms.TextInput(attrs={'readonly':'readonly', 'class': 'form-control'}))
     metadata = forms.CharField(label = '', widget=forms.TextInput(attrs={'readonly':'readonly', 'id': 'metadata', 'hidden': 'true'}))
-    return_url = forms.CharField(label = '', max_length=100, widget=forms.TextInput(attrs={'readonly':'readonly', 'id': 'return_url' , 'hidden': 'true'}))
+    return_url = forms.CharField(label = '', widget=forms.TextInput(attrs={'readonly':'readonly', 'id': 'return_url' , 'hidden': 'true'}))
