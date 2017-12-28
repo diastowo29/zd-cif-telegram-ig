64c3c183add4eb43adbdee25e34512b370fd4a37
Diastowo <diastowo@gmail.com>
2017-12-28 17:33:04 +0700

    add placeholder telegram

--- zendesk/form.py
+++ zendesk/form.py
@@ -1,20 +1,16 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    name = forms.CharField(label='Integration Name', widget=forms.TextInput(attrs={'class':'form-control'}))
-    api_id = forms.CharField(label='Api ID', widget=forms.TextInput(attrs={'class':'form-control'}))
-    api_hash = forms.CharField(label='Api Hash', widget=forms.TextInput(attrs={'class':'form-control'}))
-    phone_number = forms.CharField(label='Phone Number',  widget=forms.TextInput(attrs={'class':'form-control'}))
+    name = forms.CharField(label='Integration Name', widget=forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Integration Name'}))
+    api_id = forms.CharField(label='Api ID', widget=forms.TextInput(attrs={'class':'form-control', 'placeholder': '12345'}))
+    api_hash = forms.CharField(label='Api Hash', widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'123456abcde'}))
+    phone_number = forms.CharField(label='Phone Number',  widget=forms.TextInput(attrs={'class':'form-control', 'placeholder': '+62800000000'}))
     username = forms.CharField(label='Username',  widget=forms.TextInput(attrs={'class':'form-control'}))
 
 class MetaContactForm(forms.Form):
-    # name = forms.CharField(label='',  widget=forms.TextInput(attrs={'readonly':'readonly', 'class': 'form-control'}))
     verify = forms.CharField(label = '', widget=forms.TextInput(attrs={'class': 'form-control', 'id': 'verify'}))
     metadata = forms.CharField(label = '', widget=forms.TextInput(attrs={'readonly':'readonly', 'id': 'metadata', 'hidden': 'true'}))
     return_url = forms.CharField(label = '', widget=forms.TextInput(attrs={'readonly':'readonly', 'id': 'return_url' , 'hidden': 'true'}))
-    # api_id = forms.CharField(label='Api ID',  widget=forms.TextInput(attrs={'readonly':'readonly'}))
-    # api_hash = forms.CharField(label='Api Hash',  widget=forms.TextInput(attrs={'readonly':'readonly'}))
-    # phone_number = forms.CharField(label='Phone Number',  widget=forms.TextInput(attrs={'readonly':'readonly'}))
 
 class SendMetaForm(forms.Form):
     name = forms.CharField(label='Account Name',  widget=forms.TextInput(attrs={'readonly':'readonly', 'class': 'form-control'}))
